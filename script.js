4b76ede8e93a166f7b62511583c8de890b268cae
Matheus <125655589+Matheusgeronimo@users.noreply.github.com>
2024-02-16 07:04:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let name;
+
+nome = prompt ("Qual teu nome?");
+console.log(nome);
+window.alert(`Tu é bagua ${nome}!`);
+let numero = 22;
+let palpite = prompt("Digite um numero de 0 a 100");
+console.log(palpite);
+if(numero ==palpite){
+    window.alert(`TU É 22 ${nome} MITOOOOO`)
+}
